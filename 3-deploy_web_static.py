fe842b2af545a6973e60bde3aaf236d9a96c641d
Jocseh07 <josephmkaranja07@gmail.com>
2024-05-07 19:33:43 +0300

    more

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -3,7 +3,6 @@
 
 import os.path
 from datetime import datetime
-from os.path import isdir
 
 from fabric.api import env, local, put, run
 
@@ -16,7 +15,7 @@ def do_pack():
     tgz = "versions/web_static_{}{}{}{}{}{}.tgz".format(
         date.year, date.month, date.day, date.hour, date.minute, date.second
     )
-    if isdir("versions") is False:
+    if os.path.isdir("versions") is False:
         if local("mkdir versions").failed is True:
             return None
     if local("tar -czvf {} web_static".format(tgz)).failed is True:
