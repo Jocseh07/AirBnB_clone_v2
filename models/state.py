c78483558fb265b4a9e28a9cc418feb7209d683b
Jocseh07 <josephmkaranja07@gmail.com>
2024-05-17 14:11:13 +0300

    7

--- models/state.py
+++ models/state.py
@@ -20,7 +20,7 @@ class State(BaseModel, Base):
         from models import storage
         from models.city import City
         city_list = []
-        for city in storage.all(City).values():
+        for city in storage.all().values():
             if isinstance(city, City) and city.state_id == self.id:
                 city_list.append(city)
         return city_list
