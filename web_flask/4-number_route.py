1b21ee4aa41ea6c034284b82fe07fe49a4c929fe
Jocseh07 <josephmkaranja07@gmail.com>
2024-05-17 12:01:51 +0300

    2

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""Flask web application."""
+
+from flask import Flask, abort
+from markupsafe import escape
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """Display Hello HBNB!"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """Display HBNB."""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def text(text):
+    """Display C then text."""
+    return f"C {escape(text).replace('_', ' ')}"
+
+
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python(text="is cool"):
+    """Display python then text."""
+    text = text.replace('_', ' ')
+    return f"Python {escape(text)}"
+
+
+@app.route("/number/<n>", strict_slashes=False)
+def number(number):
+    """Display number."""
+    if number.isdigit():
+        return f"{number} is a number"
+    else:
+        abort(404)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
