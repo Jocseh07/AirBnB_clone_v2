b2546bcb0a80d8a5b199a7cc8c3cea88e8995349
Jocseh07 <josephmkaranja07@gmail.com>
2024-06-12 09:12:51 +0300

    pycodestyle fixes

--- models/state.py
+++ models/state.py
@@ -5,7 +5,6 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
 from models.city import City
-from models import storage
 
 
 class State(BaseModel, Base):
@@ -18,6 +17,8 @@ class State(BaseModel, Base):
     @property
     def cities(self):
         """Getter attribute to returns the list of City objects."""
+        from models import storage
+        from models.city import City
         city_list = []
         result = []
         total = storage.all()
@@ -27,7 +28,7 @@ class State(BaseModel, Base):
             city = city.split()
             if city[0] == 'City':
                 city_list.append(total[city])
-        for name in city_list:
-            if (name.state_id == self.id):
-                result.append(name)
+        for city in city_list:
+            if city.state_id == self.id:
+                result.append(city)
         return result
